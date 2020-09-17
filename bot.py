e25c6baeaa406e92c13cd5e5b55948605bf47613
malakhouski <timamal.sol@gmail.com>
2020-09-17 20:17:18 +0300

    commit text

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Привет, мой создатель')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'я тебя люблю':
+    elif message.text.lower() == 'я тебя frfrf':
         bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
 
 @bot.message_handler(content_types=['sticker'])
